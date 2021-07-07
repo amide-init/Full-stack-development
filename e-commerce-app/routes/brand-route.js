9b8728cb41f916f474ab6ff87aa7401d9a9d6516
Amin Uddin <aminuddin01697@gmail.com>
2021-07-07 19:45:29 +0530

    brand route has been created

--- e-commerce-app/routes/brand-route.js
+++ e-commerce-app/routes/brand-route.js
@@ -2,7 +2,33 @@ const router = require('express').Router();
 const Brand = require('../models/brand');
 
 router.get('/', (req, res) => {
+    Brand.find({})
+         .exec()
+         .then((result) => {
+             if(result.length < 1) {
+                 return res.json({success: false, message: "No brand found"})
+             }
+             res.json({success : true, data: result})
+         })
+         .catch((err) => {
+             res.json({success: false, messsage : "server error"})
+         })
+})
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Brand.findById(id)
+         .exec()
+         .then(result => {
+             if(result) {
+                return res.json({success : true, data: result})
+             }
+             res.json({success : false, message: "Invalid ID" })
+           
+         })
+         .catch((err) => {
+            res.json({success : false, message: "Server error"})
+         })
 })
 
 router.post('/', (req, res) => {
@@ -26,12 +52,31 @@ router.post('/', (req, res) => {
 
 })
 
-router.patch('/', (req, res) => {
-    res.json("patch route")
+router.patch('/:id', (req, res) => {
+  const id =  req.params.id;
+//   const updated_data = {
+//     description:  req.body.description
+//   }
+  Brand.update({_id: id}, {$set : req.body})
+       .exec()
+       .then((_) => {
+        res.json({success: true, message: "Brand has been  updated"})
+       })
+       .catch(err => {
+           res.json({success: false, message: "Server error"})
+       })
 })
 
-router.delete('/', (req, res) => {
-    res.json("delete route")
+router.delete('/:id', (req, res) => {
+    const id =  req.params.id;
+    Brand.deleteOne({_id:id})
+         .exec()
+         .then((_) => {
+          res.json({success: true, message: "brand has been deleted"})
+         })
+         .catch(err => {
+             res.json({success: false, message: "Server error"})
+         })
 })
 
 module.exports = router;
