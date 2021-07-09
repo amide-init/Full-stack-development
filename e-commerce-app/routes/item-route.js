b8e76a263b4214ec17a4fef7aac90e2c0bdced94
Amin Uddin <aminuddin01697@gmail.com>
2021-07-09 17:49:00 +0530

    item  route has been completed

--- e-commerce-app/routes/item-route.js
+++ e-commerce-app/routes/item-route.js
@@ -1,15 +1,74 @@
 const router = require('express').Router();
-
+const Item = require('../models/item');
 router.get('/', (req, res) => {
-    res.json("get route")
+    Item.find({})
+        .exec()
+        .then((result) => {
+            if (result.length < 1) {
+                return res.json({ success: false, message: "No Item Found" });
+            }
+            res.json({ success: true, data: result })
+        })
+        .catch((err) => {
+            res.json({ success: false, message: "Server error" })
+        })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Item.findById(id)
+         .exec()
+         .then(result => {
+             if(result) {
+                return res.json({success : true, data: result})
+             }
+             res.json({success : false, message: "Invalid ID" })
+           
+         })
+         .catch((err) => {
+            res.json({success : false, message: "Server error"})
+         })
+})
 router.post('/', (req, res) => {
-    res.json("post route")
+    const item = new Item({
+        title: req.body.title,
+        description: req.body.description,
+        thumbnail: req.body.thumbnail,
+        category: req.body.category,
+        brand: req.body.brand,
+        price: req.body.price,
+    });
+
+    item.save()
+        .then((_) => {
+            res.json({ success: true, message: "Item has been created" })
+        })
+        .catch((err) => {
+            res.json({ success: false, message: "server error" })
+        })
 })
 
-router.patch('/', (req, res) => {
-    res.json("patch route")
-})
+router.patch('/:id', (req, res) => {
+    const id = req.params.id;
 
-module.exports =  router;
+    Item.update({ _id: id }, { $set: req.body })
+        .exec()
+        .then((_) => {
+            res.json({ success: true, message: "Item has been  updated" })
+        })
+        .catch(err => {
+            res.json({ success: false, message: "Server error" })
+        })
+})
+router.delete('/:id', (req, res) => {
+    const id =  req.params.id;
+    Item.deleteOne({_id:id})
+         .exec()
+         .then((_) => {
+          res.json({success: true, message: "Item has been deleted"})
+         })
+         .catch(err => {
+             res.json({success: false, message: "Server error"})
+         })
+})
+module.exports = router;
